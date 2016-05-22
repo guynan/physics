7b8f90b2228d1c33e0c9576665afc9716b3462d9
Guy Nankivell <guy.nankivell@gmail.com>
2016-05-22 21:24:05 +1200

    This tidies up some stuff in terms of code aesthetics

--- straight_line_plots.py
+++ straight_line_plots.py
@@ -10,34 +10,36 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 
-filename = 'test_data.txt'
+class ImportTest():
+   
+    def data(filename):
+        file_object = (open(filename, 'r')).readlines()
+        xdata = []
+        ydata = [] 
+        for line in file_object:
+            line = line.split(";")
+            xdata.append(float(line[0]))
+            ydata.append(float(line[1]))
+        return xdata,ydata
+    
+    filename = 'test_data.txt'
+    x,y = data(filename)
 
-def import_test_data(filename):
-    file_object = (open(filename, 'r')).readlines()
-    xdata = []
-    ydata = [] 
-    for line in file_object:
-        line = line.split(";")
-        xdata.append(float(line[0]))
-        ydata.append(float(line[1]))
-    return xdata,ydata
- 
-x,y = import_test_data(filename) 
 
-#x = array([1,2,3,4,5,6,7])
-#y = array([2,4,6,9,10,11,15]) 
+def main():
 
-def func(x, a, b):
-    return a*x + b
+    x = ImportTest.x
+    y = ImportTest.y
 
-def lregress(x, y):
-    a,b = curve_fit(func, x, y)[0] 
-    return [b,a]
+    def func(x, a, b):
+        return a*x + b
 
-print(lregress(x,y))
+    def lregress(x, y):
+        a,b = curve_fit(func, x, y)[0] 
 
-"""
-from straight_line_plots import lregress
-from straight_line_plots import import_test_data
+        return [b,a]
 
-"""
+    print(lregress(x, y)) 
+
+main()
+
