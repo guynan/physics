cc972687342243d13148ca91d873ffe7857b2c72
Guy Nankivell <guy.nankivell@gmail.com>
2016-05-22 16:15:12 +1200

    This adds matrix regress functionality

--- linear_least_squares.py
+++ linear_least_squares.py
@@ -1,1 +1,28 @@
-# Third component
+"""
+Linear least-squares model fitting
+
+"""
+
+import numpy
+import math
+from pylab import *
+import matplotlib.pyplot as plt
+from straight_line_plots import import_test_data
+
+def mregress(x,y):
+    A = column_stack((array(x),array(y)))
+    result = solve(dot(transpose(A),A),dot(transpose(A),y))
+    return result
+
+def main():
+    filename = 'test_data.txt'
+    x,y = import_test_data(filename)
+
+    print(mregress(x,y))
+
+main()
+
+"""
+from linear_least_squares import mregress 
+
+"""
