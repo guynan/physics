2048ac169f3ab333ece58c92435cf1dc1fc28a5f
Guy Nankivell <guy.nankivell@gmail.com>
2016-05-26 16:40:14 +1200

    This is pretty goddamn finished in terms of logic, aesthetic changes may follow

--- weighted_least_squares.py
+++ weighted_least_squares.py
@@ -1,1 +1,54 @@
-# Weighted least squares fitting to a straight line
+"""
+Weighted least squares fitting to a straight line
+
+"""
+
+import numpy as np
+import math
+from pylab import *
+import matplotlib.pyplot as plt
+
+
+
+def wregress(x,y,sy):
+    sum_x_y = []
+    sum_x_sy2 = []
+    sum_x2_sy2 = []
+    sy_inv = []
+    sum_y_sy2 = []
+    n = 0
+    for i in x:
+        sum_x_sy2.append(i* (float(sy[n])**-2))
+        sum_x2_sy2.append((i**2)/float(sy[n])**2)
+        sum_x_y.append((i * float(y[n])/
+                            float(sy[n])**2))
+        sum_y_sy2.append(float(y[n])/
+                        float(sy[n])**2)
+        sy_inv.append(float(sy[n])**-2)
+        n += 1 
+    
+    A = column_stack((
+                [sum(sum_x2_sy2),sum(sum_x_sy2)],
+                [sum(sum_x_sy2),sum(sy_inv)]))
+    b = [sum(sum_x_y),sum(sum_y_sy2)]
+    a,b = dot(inv(A),b)
+
+    return [a,b]
+
+
+
+x = arange(0,5,0.5)
+y = array([2.75,4.83,5.05,6.80,8.83,
+            8.64,11.03,13.20,13.08,15.68]
+            )
+sy = array([0.5,0.6,0.7,0.8,0.9,0.9,
+                    1.0,1.1,1.1,1.2]
+            )
+
+c = wregress(x,y,sy)
+print(c)
+plot(x,polyval(c,x),x,y,'o')
+show()
+
+
+
